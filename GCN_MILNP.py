e932231a3e6a3e50d05c2fe9055958c026972f76
thjnmcdonald <t.h.j.n.mcdonald@students.uu.nl>
2022-07-04 11:25:11 +0200

    lest see

--- GCN_MILNP.py
+++ GCN_MILNP.py
@@ -178,11 +178,10 @@ def make_input_constraints(m: gp.Model, rel_data, n, F, feature_map, hard_coded 
              for i in range(n)), name = '8h')
 
         m.addConstrs((A[i,j] == A[j,i] for i in range(n) for j in range(n)), name='symmetry_breaker')
-
         
         # we need to have connected graph
         # since A[0,0] and A[1,1] are one, we only need this for i >= 1
-        m.addConstrs((A[i,i] <= gp.quicksum(A[j,i] for j in range(i)) for i in range(2, n)), name = 'conected_graph')
+        m.addConstrs((A[i,i] <= gp.quicksum(A[j,i] for j in range(i)) for i in range(1, n)), name = 'conected_graph')
 
     return m, A, x
 
